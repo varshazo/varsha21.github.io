210c5175b1816db6b49e678146923c0fde253e92
varshazo <83534726+varshazo@users.noreply.github.com>
2021-06-15 15:54:34 +0530

    Update script.js

--- mod4/script.js
+++ mod4/script.js
@@ -1,1 +1,22 @@
-
+var names=new Array();
+names[0]="Yaakov";
+names[1]="John";
+names[2]="Jen";
+names[3]="Jason";
+names[4]="Paul";
+names[5]="Frank";
+names[6]="Larry";
+names[7]="Paula";
+names[8]="Laura";
+names[9]="Jim";
+for(var i=0;i<names.length;i++)
+{
+  if(names[i].charAt(0)==='j'||names[i].charAt(0)==='J')
+  {
+    console.log("Goodbye "+names[i])
+  }
+  else
+  {
+    console.log("Hello "+names[i])
+  }
+}
